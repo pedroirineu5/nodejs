fc360b18a224acce8cfb9b27d6421fc030b4cde7
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-03-15 18:06:30 -0300

    feat(studies): comecando com modules

---
+++ aula1/mod1.js
@@ -0,0 +1,14 @@
+const nome = 'Pedro'
+const sobrenome = 'Henrique'
+const idade = 19
+
+const falaNome =  () =>{
+    console.log(`Olá ${nome} ${sobrenome} e tenho ${idade} anos de idade`)
+}
+
+exports.name = nome
+exports.pairname = sobrenome
+exports.falaNome = falaNome
+exports.idade = idade
+this.qualquerCoisa = 'Qualquer coisa'
+
