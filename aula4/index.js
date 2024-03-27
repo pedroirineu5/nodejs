b5f80626213c71f95a292e513af5413aafb54986
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-03-27 18:19:54 -0300

    feat(style): mandando novos assuntos, aprendi o básico de regex.

--- aula4/index.js
+++ aula4/index.js
@@ -11,9 +11,21 @@ async function walk(files, rootDir){
     for(let file of files){
         const fileFullPath = path.resolve(rootDir, file)
         const stats = await fs.stat(fileFullPath)
-        console.log(file, stats.isDirectory())
-    }
-}
+     
+        if (stats.isDirectory()){
+            lerDiretorio(fileFullPath)   
+        }
 
-lerDiretorio('../')// tem um mini delay
+
+        //criando expressão regular para evitar que printe os arquivo/diretorios de .git e node_modules
+
+        if(/\.git/g.test(fileFullPath)) continue
+        if(/\node_modules/g.test(fileFullPath)) continue
+        if(!/\.css$/g.test(fileFullPath)) continue
+
+        console.log(file)
+    }
+} 
+ 
+lerDiretorio('./')// tem um mini delay
 console.log('roda primeiro, porque é mais rápido que a funcao async')
