e26cac9adbf8be2d978e80fe61fa7113124d9e17
pedroirineu <pedrohenriqueirineu@gmail.com>
2024-04-06 21:18:41 -0300

    dfjkgsndfjkgdfnsjk

---
+++ aula7/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send(`
+    <form action="/" method="POST">
+    nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `)
+})
+
+app.post('/',(req,res)=>{
+    res.send('penis')
+})
+
+app.get('/sobre', (req,res)=>{
+    res.send('HTML DA PÁGINMA SOBRE OBRIGADO POR ENTRAR EM CONTATO COM A GENTE')
+})
+
+app.listen(3000,()=>{
+    console.log('O servidor está iniciando...')
+    console.log('servidor iniciado: http://localhost:3000')
+})
+
+// nessa aula vou aprender a como usar o nodemon, para não ficar ligando e desligando o servidor o tempo todo
