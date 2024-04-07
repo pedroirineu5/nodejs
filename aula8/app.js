a3fff22965acf700d99f1d62e77dc07d58f8d600
pedroirineu <pedrohenriqueirineu@gmail.com>
2024-04-06 22:23:22 -0300

    gdfsgfdsmkl

--- aula8/app.js
+++ aula8/app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res)=>{
-    res.send('Agora vai edition, consegui configurar junto com o nodemon o projeto')
+app.get('/',(req,res)=>{
+    res.send(`
+    <form action="/" method="POST">
+    nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `)
 })
 
-app.listen(3000, ()=>{
+app.post('/',(req,res) => {
+    res.send('formulário enviado')
+})
+
+app.get('/tests',(req,res) => {
+    console.log(req.params)
+    res.send('oi')
+})
+
+app.listen(3000, () => {
     console.log('Starting server at: http://localhost:3000')
 })
-
