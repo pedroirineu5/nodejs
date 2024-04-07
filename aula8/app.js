4267df92e3b17a84dc2071566984b19d44277d08
pedroirineu <pedrohenriqueirineu@gmail.com>
2024-04-06 21:35:02 -0300

    gmfsdklgmfdklsgmfdkls

--- aula8/app.js
+++ aula8/app.js
@@ -5,7 +5,7 @@ app.get('/', (req,res)=>{
     res.send('Agora vai edition, consegui configurar junto com o nodemon o projeto')
 })
 
-
 app.listen(3000, ()=>{
     console.log('Starting server at: http://localhost:3000')
 })
+
