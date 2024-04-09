bd6280c1f0d898c437b832c39691222f2d28d630
pedroirineu <pedrohenriqueirineu@gmail.com>
2024-04-08 21:41:02 -0300

    fsdamfkdlsamfkdlsam

--- aula8/app.js
+++ aula8/app.js
@@ -12,12 +12,15 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/',(req,res) => {
-    res.send('penis')
+    res.send('penis') // res.send é usado para mostrar o 
 })
 
-app.get('/tests',(req,res) => {
-    console.log(req.params) // isso vai logar um objeto vazio
-    res.send('oi')
+
+//              v quando você está se referindo a um parametro opcional se usa :
+app.get('/tests/:userid?/:parametroopcional?',(req,res) => {
+    console.log(req.params) // isso vai logar um objeto vazio, ou seja, conseguimos fazer várias besteirinhas.
+    res.send(req.params) // aqui está printando no site o ID do usuário que estava na URL.
+    // res.send() // mostrando o parametro adicional e opcional no site
 })
 
 app.listen(3000, () => {
