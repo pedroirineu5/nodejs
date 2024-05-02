d89f3fc2abaeeb098f5d65e8d867b679bae11546
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-05-02 16:49:38 -0300

    fgdmasklfsdmaklfdmsakl

--- aula12/server.js
+++ aula12/server.js
@@ -7,14 +7,20 @@ app.use(
         {extended: true}
     )
 )
-// literalmente um comando para pedir ao express para usar as minhas rotas criadas
+// para rodar o conteúdo estático
+app.use(express.static('./public'))
 
+
+// para rodas as views
 app.set('views', './src/views')
+// rodar um html com aqueles if's da vida
 app.set('view engine', 'ejs')
 
 
+// literalmente um comando para pedir ao express para usar as minhas rotas criadas
 app.use(routes)
 
+//mensagem de inicio
 app.listen(3000, () => {
     console.log('Starting server at: http://localhost:3000')
     console.log('Servidor executando na porta 3000')
