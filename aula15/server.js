4d99886aa210d1a79061aa008cb549d6057c15d3
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-05-22 15:39:56 -0300

    fgdmkglfdmsglfdsmglfdsjglçfdsjgkfldsçjklç

--- aula15/server.js
+++ aula15/server.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
 
 const routes = require('./routes');
 const path = require('path');
@@ -16,6 +21,19 @@ const { middlewareGlobal } = require('./src/middlewares/middleware');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const configSession = session({
+  secret: 'mgklfdsgmdflskmklmklbvcxmbklvcx',
+  store: new MongoStore.create({ mongoUrl:process.env.CONNECTIONSTRING}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+})
+app.use(configSession)
+app.use(flash())
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
