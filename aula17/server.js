7f3c6bfa6b174042bc6f0debdff372fa79fa4017
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-05-24 15:04:46 -0300

    adfasfdsa

--- aula17/server.js
+++ aula17/server.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const flash = require('connect-flash')
-
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware');
+const protecao = require('helmet')
+const csrf = require('csurf')
+const { middlewareGlobal, checkErrorCSRF, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(protecao())
+
+//postar formularios para dentro da nossa aplicação
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const configSession = session({
@@ -30,7 +31,7 @@ const configSession = session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true
-  }
+  }  
 })
 app.use(configSession)
 app.use(flash())
@@ -38,10 +39,13 @@ app.use(flash())
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf())
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkErrorCSRF)
+app.use(csrfMiddleware)
 app.use(routes);
-
+  
 app.on('pronto', () => {
   app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
--- aula17/src/middlewares/middleware.js
+++ aula17/src/middlewares/middleware.js
@@ -6,3 +6,14 @@ exports.middlewareGlobal = (req, res, next) => {
 exports.outroMiddleware = (req, res, next) => {
    next();
 };
+
+exports.checkErrorCSRF = (e, req, res, next)=>{
+    if(e && e.code === "EBADCSRFTOKEN"){
+        return res.render('404')
+    }
+}
+
+exports.csrfMiddleware = (req, res, next) =>{
+    res.locals.csrfToken = req.csrfToken()
+    next()
+}
